21eccf0dd78319820bd7262cf71c4596401a0917
Jezmora-duh <jmora18@ivytech.edu>
2024-12-07 22:35:51 -0600

    Add files via upload

--- node-crash-course/app.js
+++ node-crash-course/app.js
@@ -1,29 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for request
 app.listen(3000);
 
+//middelware & Static files
+app.use(express.static('public'))
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-    //res.send('<p> home page </p>');
-    res.sendFile('./views/index.html', { root: __dirname})
+    const blogs = [
+        {title: 'omg hi', snippet:'Chupapimunano'},
+        {title: 'How are u', snippet:'great'},
+        {title: 'Good', snippet:'RIGHT'},
+    ];   
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    //res.send('<p> about page </p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about', { title: 'About'});
 });
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('about');
+app.get('/blogs/create', (req,res) => {
+    res.render('create', { title: 'Create new blog'});
 });
 
 //404 page-must go at the bottom
-
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404', { title: '404'});
 });
